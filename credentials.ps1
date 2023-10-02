71c073c880cc71e30d74368d49a73dcc04260beb
ImeneYAHIAOUI <90778346+ImeneYAHIAOUI@users.noreply.github.com>
2023-10-02 22:27:23 +0200

    add credentials in secret script

---
+++ credentials.ps1
@@ -0,0 +1,11 @@
+# credentials.ps1
+
+$env:PROJECT_ID = ""
+$env:REGION = ""
+$env:CONNECTION_NAME = ""
+
+$env:POSTGRESQL_ADDON_DB = ""
+$env:POSTGRESQL_ADDON_HOST = ""
+$env:POSTGRESQL_ADDON_PASSWORD = ""
+$env:POSTGRESQL_ADDON_PORT = ""
+$env:POSTGRESQL_ADDON_USER = ""
