f9eea80cc6602c559a4cf18c4b6d9fb892c4ebff
ImeneYAHIAOUI <90778346+ImeneYAHIAOUI@users.noreply.github.com>
2023-10-03 22:44:21 +0200

    fix storage module bug

--- stories-service/src/storage.module.ts
+++ stories-service/src/storage.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StorageController } from './controllers/storage.controller';
 import { StorageService } from './services/storage.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [StorageController],
   providers: [StorageService],
 })
