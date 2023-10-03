c67dbc5dafdf72168b834ac7dd78d6bb601f8b57
sourour9 <sourour.gazzeh@outlook.fr>
2023-10-03 22:03:19 +0200

    get contacts

--- users-service/src/main.ts
+++ users-service/src/main.ts
@@ -1,16 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // Ajout
+
 const HOST = '0.0.0.0';
 const PORT = Number(process.env.PORT) || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Votre API')
+    .setDescription('Description de votre API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT, HOST).then(() => {
-    // tslint:disable-next-line:no-console
     console.log(
       `** Nest Live Development Server is listening on ${HOST}:${PORT}, open your browser on http://localhost:${PORT}/ **`,
     );
   });
 }
+
 bootstrap();
