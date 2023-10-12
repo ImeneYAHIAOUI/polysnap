b5fe17e16225e53e907174a200671a13b7e08c09
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2023-10-13 00:16:25 +0200

    add corse

--- messaging-service/src/main.ts
+++ messaging-service/src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // Ajout
+
+const HOST = '0.0.0.0';
+const PORT = 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
-  //console.log(process.env.REDIS_PORT);
+  app.enableCors();
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Votre API')
+    .setDescription('Description de votre API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT, HOST).then(() => {
+    console.log(
+      `** Nest Live Development Server is listening on ${HOST}:${PORT}, open your browser on http://localhost:${PORT}/ **`,
+    );
+  });
 }
+
 bootstrap();
