604e63abb717dddbe36dd5c3e021a604d6ee3a08
Badr Al Achkar <alachkarbadr@gmail.com>
2023-10-14 02:09:33 +0200

    fix : bbackend problems

--- messaging-service/dist/app.module.js
+++ messaging-service/dist/app.module.js
@@ -15,24 +15,26 @@ const chat_service_1 = require("./services/chat.service");
 const axios_1 = require("@nestjs/axios");
 const typeorm_1 = require("@nestjs/typeorm");
 const chat_entity_1 = require("./entities/chat.entity");
+const user_entity_1 = require("./entities/user.entity");
+const user_proxy_service_1 = require("./services/user.proxy.service");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [axios_1.HttpModule,
-            typeorm_1.TypeOrmModule.forFeature([chat_entity_1.Chat]),
+            typeorm_1.TypeOrmModule.forFeature([chat_entity_1.Chat, user_entity_1.User]),
             typeorm_1.TypeOrmModule.forRoot({
                 type: 'postgres',
                 host: '/cloudsql/cloud-398911:us-central1:polysnap',
                 port: 5432,
                 username: 'user',
                 password: 'storypassword',
-                database: 'messages',
+                database: 'message',
                 entities: [__dirname + '/**/*.entity{.ts,.js}'],
                 synchronize: true,
             }),],
-        providers: [messages_service_1.MessageService, chat_service_1.ChatService],
+        providers: [messages_service_1.MessageService, chat_service_1.ChatService, user_proxy_service_1.UserProxyService],
         controllers: [messages_controller_1.MessageController, chats_controller_1.ChatController],
     })
 ], AppModule);
--- messaging-service/dist/main.js
+++ messaging-service/dist/main.js
@@ -2,9 +2,27 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
+const HOST = '0.0.0.0';
+const PORT = 8080;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(8080);
+    app.enableCors();
+    app.use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    });
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Votre API')
+        .setDescription('Description de votre API')
+        .setVersion('1.0')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    await app.listen(PORT, HOST).then(() => {
+        console.log(`** Nest Live Development Server is listening on ${HOST}:${PORT}, open your browser on http://localhost:${PORT}/ **`);
+    });
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
